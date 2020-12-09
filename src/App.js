83504b69d03c4a2dc068f7b4c3bbc58523ab4d30
emr404 <roufaym@gmail.com>
2020-12-09 21:26:13 +0000

    Added routers and navigation

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
 import logo from './logo.svg';
-import './App.css';
+import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
+import Header from "./pages/Header";
+import Basket from "./pages/Basket";
+import Products from "./pages/Products";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <Router>
+            <Header/>
+            <Switch>
+                <Route path = '/' exact component={Products}/>
+                <Route path = '/Basket' component={Basket}/>
+            </Switch>
+      </Router>
     </div>
   );
 }
---
+++ src/pages/Header.js
@@ -0,0 +1,26 @@
+import React from 'react'
+import { Link } from "react-router-dom";
+const Header = () => {
+    return (
+        <div className='Header'>
+            <div className='Logo'>
+                <h2>KTS</h2>
+            </div>
+
+            <div className='NavLinks'>
+
+                <Link to='/' className='NavLink'>
+                    Shop
+                </Link>
+                
+                <Link to='/Basket' className='NavLink'>
+                    Your Basket
+                </Link>
+            </div>
+
+            
+        </div>
+    )
+}
+
+export default Header
