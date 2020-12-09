5d2152ac79dcd922864ddd3d472b6cc769b35b8b
emr404 <roufaym@gmail.com>
2020-12-09 21:34:47 +0000

    Added display of products

--- src/pages/Products.js
+++ src/pages/Products.js
@@ -1,9 +1,27 @@
-import React from 'react'
+import React,{useState} from 'react'
+import { ProductData, ProductsData } from "../data/products.js";
 
 const Products = () => {
+    const [Products, setProducts] = useState(ProductData)
     return (
-        <div>
-            <h1>Product</h1>
+        <div className='ProductsContainer'>
+        
+        {
+            Products.map(product=>
+            <div className='Product'>
+            <form onSubmit=''>
+            <img src={product.image.default} alt={product.title} title={product.title}/>
+            <p id='Title'>{product.title}</p>
+                    <p id='Price'>£{product.price}</p>
+                    
+                    
+                    <button>Add to Basket</button>
+            </form>
+
+            </div>
+            )
+        }
+            
         </div>
     )
 }
