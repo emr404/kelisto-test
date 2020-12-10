5fa8ad037135cccf10cf0ea1e119a4e459da9828
emr404 <roufaym@gmail.com>
2020-12-10 00:08:29 +0000

    Added basket counter

--- src/pages/Header.js
+++ src/pages/Header.js
@@ -1,7 +1,9 @@
-import React from 'react'
+import React, {useContext } from 'react'
 import { Link } from "react-router-dom";
+import {ProductContext} from '../context/BasketContext';
 import "../styling/style.scss";
 const Header = () => {
+    const [basketContent, setBasketContent] = useContext(ProductContext)
     return (
         <div className='Header'>
         
@@ -15,7 +17,7 @@ const Header = () => {
                 </Link>
                 
                 <Link to='/Basket' className='NavLink'>
-                    Your Basket
+                    Your Basket ({basketContent.length == 1 ? basketContent.length + ' item' : basketContent.length + ' items' })
                 </Link>
             </div>
 
